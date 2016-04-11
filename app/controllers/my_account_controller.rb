1936e59bf5ffed7faf7748c1b99da7c9a05adef5
poosan9@gmail.com <poosan9@gmail.com>
2016-04-11 10:30:05 +0530

    Added Some Features in admin page

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -1,5 +1,10 @@
 class MyAccountController < ApplicationController
   def show
 
+    id=@current_user.cash_account.id
+    @amount_given_to_cashier = AccountEntry
+                                   .where(:type => 'AccountEntry::Credit')
+                                   .where(:account_id => id)
+                                   .order("created_at")
   end
 end
