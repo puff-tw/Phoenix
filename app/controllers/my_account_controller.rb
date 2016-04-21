175302d54f93fa928c3f34ca776698ea4680f618
poosan9@gmail.com <poosan9@gmail.com>
2016-04-21 12:55:52 +0530

    Added Card Transcation Summary in myaccount screen

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -4,6 +4,7 @@ class MyAccountController < ApplicationController
     @debit = @current_user.cash_account.entries.debit_detail
     @credit = @current_user.cash_account.entries.credit_detail
 
+
     id=@current_user.cash_account.id
     @amount_given_to_cashier = AccountEntry
                                    .where(:mode => 'Account::BankAccount')
@@ -11,11 +12,34 @@ class MyAccountController < ApplicationController
                                    .where(:account_id => id)
                                    .order("created_at DESC")
 
+
+    # SELECT *
+    #     FROM account_entries
+    # LEFT OUTER JOIN account_txns
+    # ON account_txns.id = account_entries.account_txn_id
+    # LEFT OUTER JOIN users
+    # ON users.id = account_txns.created_by_id
+    # WHERE mode = 'Account::BankAccount' AND account_entries.type = 'AccountEntry::Debit'
+    # AND users.id = 1;
+
+    @card = AccountEntry
+              .joins(:account_txn => :created_by)
+              .where(:mode => 'Account::BankAccount')
+              .where(:type => 'AccountEntry::Debit')
+              .where('users.id=?', @current_user.id)
+              .group("date(account_entries.created_at)")
+              .sum(:amount)
+
+
     allDates = Array.new
     @debit.each do |k, v|
       allDates << k
     end
     @credit.each do |k, v|
+      allDates << k
+    end
+
+    @card.each do |k, v|
       allDates << k
     end
 
